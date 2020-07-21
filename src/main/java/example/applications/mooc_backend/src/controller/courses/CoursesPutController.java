da0eb76ad49648088749a4a0a536093e1085f23d
Eduard Carvajal <efcarvajal10@gmail.com>
2020-07-20 19:06:35 -0500

    applying DDD... course codely-tv

--- src/main/java/example/applications/mooc_backend/src/controller/courses/CoursesPutController.java
+++ src/main/java/example/applications/mooc_backend/src/controller/courses/CoursesPutController.java
@@ -1,7 +1,5 @@
 package example.applications.mooc_backend.src.controller.courses;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.PathVariable;
